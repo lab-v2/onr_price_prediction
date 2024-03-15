e232f3d1e89d51ebc8d7bec149b6db45ab946df7
Ezra Lee <manimateus99@gmail.com>
2024-03-15 00:47:41 -0700

    feat: added EDCR results

---
+++ analysis/edcr/example.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+import numpy as np
+import pandas as pd
+from src.rule_correction import *
+
+# load data
+METRIC = 'precision'
+
+with open(f'data/test_{METRIC}.npy', 'rb') as f:
+    data = np.load(f, allow_pickle=True)
+
+results = []
+epsilon = [0.001 * i for i in range(1, 100, 1)]
+for ep in epsilon:
+    #result = PosNegRuleLearn(all_charts, epsilon)
+    result = ruleForNegativeCorrection(data, ep)
+    results.append([ep] + result)
+    print(f"ep:{ep}\n{result}")
+col = ['pre', 'recall', 'F1', 'NSC', 'PSC', 'NRC', 'PRC']
+df = pd.DataFrame(results, columns = ['epsilon'] + col )
+df.to_csv(f"rule_for_Negativecorrection_{METRIC}.csv")
+
+results = []
+for ep in epsilon:
+    #result = PosNegRuleLearn(all_charts, epsilon)
+    result = ruleForNPCorrection(data, ep)
+    results.append([ep] + result)
+    print(f"ep:{ep}\n{result}")
+col = ['pre', 'recall', 'F1', 'NSC', 'PSC', 'NRC', 'PRC']
+#df = pd.DataFrame(results, columns = ['epsilon'] + col + ['acc', 'macro-F1', 'micro-F1'])
+df = pd.DataFrame(results, columns = ['epsilon'] + col )
+df.to_csv( f"rule_for_NPcorrection_{METRIC}.csv")
+
+results = []
+for ep in epsilon:
+    #result = PosNegRuleLearn(all_charts, epsilon)
+    result = ruleForPNCorrection(data, ep)
+    results.append([ep] + result)
+    print(f"ep:{ep}\n{result}")
+df = pd.DataFrame(results, columns = ['epsilon'] + col )
+df.to_csv( f"rule_for_PNcorrection_{METRIC}.csv")
