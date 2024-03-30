51d6092a0c92443d2b36f3a03fe993697e5a9512
Ezra Lee <manimateus99@gmail.com>
2024-03-30 06:37:36 -0700

    feat: added streaming data

--- analysis/edcr_support_no_val.py
+++ analysis/edcr_support_no_val.py
@@ -40,7 +40,7 @@ import numpy as np
 
 COMMODITYS = ['cobalt', 'copper', 'germanium', 'magnesium', 'nickel']
 #target_COMMODITY = "copper"
-target_COMMODITY = "nickel"
+target_COMMODITY = "cobalt"
 WINDOW_SIZE = 20
 
 pre_features = []
@@ -57,26 +57,27 @@ data = get_data(VOLZA_FILE_PATH, PRICE_FILE_PATH, window_size=WINDOW_SIZE, cente
 
 # Add Isolation Forest spikes column
 data['spikes_if'] = utils.detect_spikes_if(data, TARGET_COLUMN, contamination=0.1)
+data['spikes_streaming'] = utils.detect_spikes_streaming(data, 'Price', window_size=WINDOW_SIZE)
     
 # Add Bowen's spike detection
-for COMMODITY in COMMODITYS:
-    VOLZA_FILE_PATH = f"../volza/{COMMODITY}/{COMMODITY}.csv"
-    PRICE_FILE_PATH = f"../volza/{COMMODITY}/{COMMODITY}_prices.csv"
+# for COMMODITY in COMMODITYS:
+#     VOLZA_FILE_PATH = f"../volza/{COMMODITY}/{COMMODITY}.csv"
+#     PRICE_FILE_PATH = f"../volza/{COMMODITY}/{COMMODITY}_prices.csv"
     
-    # Get the data
-    data = get_data(VOLZA_FILE_PATH, PRICE_FILE_PATH, window_size=WINDOW_SIZE, center=False)
+#     # Get the data
+#     data = get_data(VOLZA_FILE_PATH, PRICE_FILE_PATH, window_size=WINDOW_SIZE, center=False)
     
-    # Add Isolation Forest spikes column
-    data['spikes_if'] = utils.detect_spikes_if(data, TARGET_COLUMN, contamination=0.1)
+#     # Add Isolation Forest spikes column
+#     data['spikes_if'] = utils.detect_spikes_if(data, TARGET_COLUMN, contamination=0.1)
     
-    # Add Bowen's spike detection
-    _, features, labels = utils.detect_spikes_new(data, TARGET_COLUMN, window_size=WINDOW_SIZE, center=False)
-    if COMMODITY == target_COMMODITY:
-        tar_features.extend(features)
-        tar_labels.extend(labels)
-        continue
-    pre_features.extend(features)
-    pre_labels.extend(labels)
+#     # Add Bowen's spike detection
+#     _, features, labels = utils.detect_spikes_new(data, TARGET_COLUMN, window_size=WINDOW_SIZE, center=False)
+#     if COMMODITY == target_COMMODITY:
+#         tar_features.extend(features)
+#         tar_labels.extend(labels)
+#         continue
+#     pre_features.extend(features)
+#     pre_labels.extend(labels)
 
 
 
@@ -106,10 +107,12 @@ sampler = RandomOverSampler
 #X_test_price_mix, y_test_price_mix = data_processing.create_sequences(X_test_price_mix, y_test_price_mix, WINDOW_SIZE)
 #X_val_mix, y_val_mix = data_processing.create_sequences(X_val_mix, y_val_mix, WINDOW_SIZE)
 
+keyword = 'streaming'
+
 # Evaluate and create pre-trained model
-output_file_path = f'{target_COMMODITY}_no_val_{WINDOW_SIZE}/test/results_test.csv'
-pred_file_path = f'{target_COMMODITY}_no_val_{WINDOW_SIZE}/test/predictions/test'
-model_path = f'{target_COMMODITY}_no_val_{WINDOW_SIZE}/best_model'
+output_file_path = f'{target_COMMODITY}_{keyword}_{WINDOW_SIZE}/test/results_test.csv'
+pred_file_path = f'{target_COMMODITY}_{keyword}_{WINDOW_SIZE}/test/predictions/test'
+model_path = f'{target_COMMODITY}_{keyword}_{WINDOW_SIZE}/best_model'
 # X_train_price_mix = np.expand_dims(X_train_price_mix, axis = 2)
 # X_test_price_mix = np.expand_dims(X_test_price_mix, axis = 2)
 # X_val_mix = np.expand_dims(X_val_mix, axis = 2)
@@ -129,9 +132,9 @@ print(pred_file_path)
 
 
 # Prepare price data
-# X_price, y_price = data_processing.prepare_features_and_target(data, TARGET_COLUMN, 'spikes')
-X_price = np.array(tar_features)
-y_price = np.array(tar_labels)
+X_price, y_price = data_processing.prepare_features_and_target(data, TARGET_COLUMN, 'spikes_streaming')
+# X_price = np.array(tar_features)
+# y_price = np.array(tar_labels)
 
 # Split price data
 X_train_price, X_test_price, y_train_price, y_test_price = train_test_split(X_price, y_price, test_size=0.4, shuffle=False)
@@ -144,13 +147,13 @@ X_train_price, y_train_price = sampler(random_state=RANDOM_STATE).fit_resample(X
 X_train_price, X_test_price = data_processing.scale_features_no_val(X_train_price, X_test_price)
 
 # Sequence making
-# X_train_price, y_train_price = data_processing.create_sequences(X_train_price, y_train_price, WINDOW_SIZE)
-# X_test_price, y_test_price = data_processing.create_sequences(X_test_price, y_test_price, WINDOW_SIZE)
+X_train_price, y_train_price = data_processing.create_sequences(X_train_price, y_train_price, WINDOW_SIZE)
+X_test_price, y_test_price = data_processing.create_sequences(X_test_price, y_test_price, WINDOW_SIZE)
 # X_val_price, y_val_price = data_processing.create_sequences(X_val_price, y_val_price, WINDOW_SIZE)
 
 # Use this for Bowen's method
-X_train_price = np.expand_dims(X_train_price, axis = 2)
-X_test_price = np.expand_dims(X_test_price, axis = 2)
+# X_train_price = np.expand_dims(X_train_price, axis = 2)
+# X_test_price = np.expand_dims(X_test_price, axis = 2)
 
 results_df  = models.evaluate_all(X_train_price, y_train_price, None, None, X_test_price, y_test_price, output_file_path, pred_file_path, model_path, False, val=False)
 
